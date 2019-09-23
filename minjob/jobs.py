8f7d09edb45f980cc6e334bd6a94b1a20d66598d
Mario Dagrada <mariodagrada24@gmail.com>
2019-09-23 22:37:05 +0200

    [WIP] Added a method to terminate the running processes
    This commit also makes the monitoring function optionally
    non-blocking by executing the monitoring on a separate thread.

--- minjob/jobs.py
+++ minjob/jobs.py
@@ -1,38 +1,43 @@
 import time
+import ctypes
 from multiprocessing import Process
 from threading import Thread
 from minjob.logger import logger
 from minjob.logger import MailNotifier
 
-RETRY_SLEEP_TIME = 300
-JOB_MONITOR_TIME = 10
+RETRY_SLEEP_TIME = 10
+JOB_MONITOR_TIME = 2
 
 
 class Job:
-    def __init__(self, name, target, *args):
+    def __init__(self, name, target, *args, daemonize=False):
         self.name = name
         self.target = target
         self.args = args
         self.job = None
         self.id = None
         self.nfail = 0
+        self.daemonize = daemonize
 
     def start(self):
         raise NotImplementedError("Not implemented")
+
+    def stop(self):
+        raise NotImplementedError("Not implement")
 
     def is_alive(self):
         is_running = self.job.is_alive()
         if not is_running:
             self.nfail = self.nfail + 1
             self.job.join()
+            self.id = None
         return is_running
 
     def __str__(self):
-        res = """
-Name: {}
-Args: {}
-pid:  {}
-        """.format(self.name, self.args, self.id)
+        res = f"""
+Name: {self.name}
+pid:  {self.id}
+"""
         return res
 
 
@@ -41,9 +46,13 @@ class JobProcess(Job):
     def start(self):
         self.job = Process(target=self.target,
                            args=self.args,
-                           name=self.name)
+                           name=self.name,
+                           daemon=self.daemonize)
         self.job.start()
         self.id = self.job.pid
+
+    def stop(self):
+        self.job.kill()
 
 
 class JobThread(Job):
@@ -51,35 +60,80 @@ class JobThread(Job):
     def start(self):
         self.job = Thread(target=self.target,
                           args=self.args,
-                          name=self.name)
+                          name=self.name,
+                          daemon=self.daemonize)
         self.job.start()
         self.id = self.job.ident
 
+    def stop(self):
+        """
+        The following code abruptly terminates a running thread. This has been adapted
+        from https://github.com/munshigroup/kthread
+
+        WARNING: it is a bad practice to abruptly kill a running thread since the resources
+        will not properly release and no try/except/finally will be executed
+        However this library has been conceived for bots which has to run for a long time without
+        interruption and usually stopping the threads means that the program is going to terminate.
+        """
+        if not self.job.is_alive():
+            return
+
+        exc = ctypes.py_object(SystemExit)
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(
+            ctypes.c_long(self.id), exc)
+        if res == 0:
+            raise ValueError(f"The chosen thread ID {self.id} is not existent")
+        elif res > 1:
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(self.id, None)
+            raise SystemError("PyThreadState_SetAsyncExc failed")
+
 
 class JobManager:
+
     def __init__(self, name="MyApp", mail_info=None):
+        # this list takes track of all available processes
         self.jobs = []
         self.name = name
         self.max_fails = 10
         self.mail_info = mail_info
+        self.supervisor = None
 
-    def add_process(self, name, target, *args):
-        p = JobProcess(name, target, *args)
+    def add_process(self, name, target, *args, daemonize=False):
+        p = JobProcess(name, target, *args, daemonize=daemonize)
         self.jobs.append(p)
 
-    def add_thread(self, name, target, *args):
-        p = JobThread(name, target, *args)
+    def add_thread(self, name, target, *args, daemonize=False):
+        p = JobThread(name, target, *args, daemonize=daemonize)
         self.jobs.append(p)
 
-    def monitor(self):
+    def start_all(self, blocking=False):
         for p in self.jobs:
             p.start()
+        if not blocking:
+            self.supervisor = JobThread("supervisor", self._monitor, daemonize=True)
+            self.supervisor.start()
+            assert self.supervisor.id is not None
+        else:
+            self._monitor()
+
+    def stop_all(self):
+        self.supervisor.stop()
+        for p in self.jobs:
+            p.stop()
+
+    def available_jobs(self):
+        return [p.name for p in self.jobs]
+
+    def _monitor(self):
         while True:
             for p in self.jobs:
                 if not p.is_alive():
-                    logger.warning("Job {} dead, restarting...".format(p.name))
+                    logger.warning(f"Job {p.name} from the {self.name} application "
+                                   f"has failed, restarting...")
                     if p.nfail % self.max_fails == 0:
-                        info = "Job {} aborted".format(p.name)
+                        info = f"Job {p.name} from the {self.name} application has failed " \
+                            f"more than {self.max_fails} times.aborted"
+                        logger.critical(info)
                         if self.mail_info:
                             notifier = MailNotifier(app_name=self.name, job_name=p.name,
                                                     config=self.mail_info)
@@ -87,4 +141,3 @@ class JobManager:
                     time.sleep(RETRY_SLEEP_TIME)
                     p.start()
             time.sleep(JOB_MONITOR_TIME)
-
--- tests/test_job_monitor.py
+++ tests/test_job_monitor.py
@@ -1,5 +1,8 @@
 import unittest
+import time
 from datetime import datetime
+
+from minjob.jobs import JobManager
 
 
 def run_process(name="charlie", code="bravo"):
@@ -27,9 +30,29 @@ def run_thread(name="hawk", code="alpha"):
 class JobManagerTests(unittest.TestCase):
 
     def setUp(self):
+        self.names = ["test_process", "test_thread"]
         self.manager = JobManager()
-        self.manager.add_process("process", run_process)
-        self.manager.add_thread("thread", run_thread)
+        self.manager.add_process(self.names[0], run_process, daemonize=True)
+        self.manager.add_thread(self.names[1], run_thread, daemonize=True)
+
+    def test_available_jobs(self):
+        self.manager.start_all()
+        available_jobs = self.manager.available_jobs()
+        self.assertListEqual(available_jobs, self.names)
 
     def test_monitor(self):
-        self.manager.monitor()
+        stop = False
+        self.manager.start_all()
+        while not stop:
+            print(self.manager.available_jobs())
+            time.sleep(10)
+            self.manager.stop_all()
+            stop = True
+        job_alive = [s.job.is_alive() for s in self.manager.jobs]
+        job_alive.append(self.manager.supervisor.job.is_alive())
+        print(job_alive)
+        # j = zip(self.manager.available_jobs(), job_alive)
+        # time.sleep(5)
+        # for name, is_alive in j:
+        #     print(name, is_alive)
+        # print("supervisor", self.manager.supervisor.job.is_alive)
