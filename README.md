fdc30ff13b431ffd90317fbe18d3365215a07308
Mario Dagrada <mariodagrada24@gmail.com>
2019-09-25 22:38:54 +0200

    Preparing setup.py and README for distribution

--- README.md
+++ README.md
@@ -8,4 +8,27 @@ with high availability requirements such as market trading bots.
 
 ## Installation and Usage
 
+Install the library within your favorite virtual environment or locally using:
 
+```bash
+pip install minjob
+```
+
+A simple usage of the library for adding a monitored process and
+thread goes like:
+
+```python
+from minjob.jobs import JobManager
+
+def my_thread_func():
+    do_some_work()
+    
+def my_process_func():
+    do_other_work()
+    
+manager = JobManager(name="MyManager")
+manager.add_process("MyProcess", target=my_process_func, daemonize=True)
+manager.add_thread("MyThread", target=my_thread_func, daemonize=True)
+
+manager.start_all()
+```
--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='minjob',
-      version='0.1',
-      description='Minimal job monitor for multi-threaded Python applications',
-      author='Mario Dagrada',
-      license='MIT',
-      packages=find_packages(exclude=['tests', 'tests.*']),
-      install_requires=[])
+setup(name="minjob",
+      version="0.1.0",
+      description="Minimal job monitor for multi-threaded Python applications",
+      author="Mario Dagrada",
+      author_email="mariodagrada24@gmail.com",
+      license="MIT",
+      url="https://github.com/madagra/minjob",
+      packages=find_packages(exclude=["tests", "tests.*"]),
+      install_requires=[],
+      python_requires=">=3.7")
